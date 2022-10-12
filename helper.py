8e00221d292be366a50b31d5fdd977f45ae8f0d0
Subzy132 <subhaanh00@gmail.com>
2022-10-12 16:19:43 +0100

    added menu list

---
+++ helper.py
@@ -0,0 +1,10 @@
+# User Stories
+#1
+# AS a User I want to be able to see the menu in a formated way, so that I can order my meal.
+menu = ["Chicken Burger", "Beef Burger", "Veggie Burger", "Chips", "Chicken Wrap", "Veggie Wrap", "Coke", "Fanta", "Water"]
+print(menu)
+#2
+# AS a User I want to be able to order 3 times, and have my responses added to a list so they aren't forgotten
+
+#3
+# As a user, I want to have my order read back to me in formated way so I know what I ordered.
